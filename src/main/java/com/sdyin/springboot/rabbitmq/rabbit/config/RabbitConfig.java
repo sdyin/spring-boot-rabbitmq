a545b92995afc80e9b40fb9fb87ad26ce1344ba0
liuye <liuye@iboxpay.com>
2018-09-05 17:51:44 +0800

    MQ消费 增加主题topic模式

--- src/main/java/com/sdyin/springboot/rabbitmq/rabbit/config/RabbitConfig.java
+++ src/main/java/com/sdyin/springboot/rabbitmq/rabbit/config/RabbitConfig.java
@@ -27,15 +27,29 @@ public class RabbitConfig {
   }
 
   @Bean
-  public Queue QueueB() {
+  public Queue fanoutQueueB() {
     return new Queue(MqConstant.QUEUE_SDYIN_DEMO);
   }
 
   @Bean
-  public Queue QueueC(){
+  public Queue fanoutQueueC(){
     return new Queue(MqConstant.QUEUE_RMM_DEMO);
   }
 
+  @Bean
+  public Queue topicQueueA(){
+    return new Queue(MqConstant.QUEUE_TOPIC_DEMO_A);
+  }
+
+  @Bean
+  public Queue topicQueueB(){
+    return new Queue(MqConstant.QUEUE_TOPIC_DEMO_B);
+  }
+
+  @Bean
+  public Queue topicQueueC(){
+    return new Queue(MqConstant.QUEUE_TOPIC_DEMO_C);
+  }
   /**
    * Fanout 就是我们熟悉的广播模式或者订阅模式，给Fanout交换机发送消息
    * 绑定了这个交换机的所有队列都收到这个消息。
@@ -43,23 +57,53 @@ public class RabbitConfig {
    */
   @Bean
   FanoutExchange fanoutExchange() {
-    return new FanoutExchange("sdyinExchange");
+    return new FanoutExchange("sdyin.fanoutExchange");
   }
 
   /**
    * 队列绑定交换机
-   * @param QueueB
+   * @param fanoutQueueB
    * @param fanoutExchange
    * @return
    */
   @Bean
-  Binding bindingExchangeA(Queue QueueB, FanoutExchange fanoutExchange) {
-    return BindingBuilder.bind(QueueB).to(fanoutExchange);
+  Binding bindingExchangeA(Queue fanoutQueueB, FanoutExchange fanoutExchange) {
+    return BindingBuilder.bind(fanoutQueueB).to(fanoutExchange);
   }
 
   @Bean
-  Binding bindingExchangeB(Queue QueueC, FanoutExchange fanoutExchange) {
-    return BindingBuilder.bind(QueueC).to(fanoutExchange);
+  Binding bindingExchangeB(Queue fanoutQueueC, FanoutExchange fanoutExchange) {
+    return BindingBuilder.bind(fanoutQueueC).to(fanoutExchange);
+  }
+
+  /**
+   * topic模式交换机
+   * @return
+   */
+  @Bean
+  public TopicExchange topicExchange(){
+    return new TopicExchange(MqConstant.EXCHANGE_TOPIC_NAME);
+  }
+
+  /**
+   * 队列绑定交换机 并关联ROUTINGKEY
+   * @param topicQueueA
+   * @param topicExchange
+   * @return
+   */
+  @Bean
+  Binding bindingTopicExchangeA(Queue topicQueueA, TopicExchange topicExchange) {
+    return BindingBuilder.bind(topicQueueA).to(topicExchange).with("topic.#");
+  }
+
+  @Bean
+  Binding bindingTopicExchangeB(Queue topicQueueB, TopicExchange topicExchange) {
+    return BindingBuilder.bind(topicQueueB).to(topicExchange).with("topic.b.#");
+  }
+
+  @Bean
+  Binding bindingTopicExchangeC(Queue topicQueueC, TopicExchange topicExchange) {
+    return BindingBuilder.bind(topicQueueC).to(topicExchange).with("topic.c");
   }
 
   //配置文件配置开启ack 或者 代码注入配置
